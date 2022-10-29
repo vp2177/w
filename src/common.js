25b99c1d3e2b0f4b6b92da15962e769c3ff227ea
vp2177 <vp2177@users.noreply.github.com>
2022-10-29 22:14:09 +0100

    rate() generator

---
+++ src/common.js
@@ -0,0 +1,8 @@
+export function* rate(around = 100, variance = 5/100) {
+    let r = around;
+    while (true) {
+        const rand = Math.random() * 2 - 1
+        r = r + rand * variance;
+        yield r
+    }
+}
