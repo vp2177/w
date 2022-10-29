8d6830eae6dbe4dc49d7d1f9d4cf217635ebcd8e
vp2177 <vp2177@users.noreply.github.com>
2022-10-30 00:13:46 +0100

    myWindow() Hyper...component?

---
+++ src/windows.js
@@ -0,0 +1,9 @@
+import {h, text} from "hyperapp";
+
+export function myWindow(x = 50, y = 0, title = "Untitled", CloseWindow, child = h("span", {})) {
+    return h("div", {class: "Window"},[
+        child,
+        h("div",{class: "Window__title"}, text(title)),
+        h("button", {class: "Window__closer", onclick: CloseWindow, title: "Close"})
+    ] )
+}
