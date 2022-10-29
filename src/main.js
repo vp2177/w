a54150659af4fa8f688572420d85e673de0e193c
vp2177 <vp2177@users.noreply.github.com>
2022-10-29 20:58:29 +0100

    base style

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ console.info("Yo");
 
 const mainEl = document.getElementById("main")
 
+/**
+ * Create 
+ * - root window
+ * - movable window
+ * - about box
+ * - price ticker
+ * - demo video player
+ */
