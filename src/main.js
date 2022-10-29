f2bb43dffdd14e06b62f6c3178a5a561675a2e9d
vp2177 <vp2177@users.noreply.github.com>
2022-10-30 00:12:19 +0100

    main Hyperapp

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import {} from "./common";
 
-const mainEl = document.getElementById("main");
-console.info( mainEl);
+
+import  {app,h,text} from "hyperapp"
+import {myWindow} from "./windows"
+
+app({
+    init: {},
+    view(state) {
+        return h("div", {class: "Desktop"}, [
+            myWindow(5, 5, "Test#1", () => {}, h("mark", {}, text("...")))
+        ]) 
+    },
+    node: document.getElementById("main"),
+
+
+})
+
 
 /**
  * Create
