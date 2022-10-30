e76c480c4737ad29c7901c37188059ba7e4cc6ee
vp2177 <vp2177@users.noreply.github.com>
2022-10-30 02:22:18 +0000

    rewrite window to (P)react

--- src/windows.jsx
+++ src/windows.jsx
@@ -1,18 +1,21 @@
+import  {h} from "preact"
+import {useState} from "preact/hooks"
 
-export function myWindow(
+
+export function MyWindow({
   x = 0,
   y = 0,
   title = "Untitled",
-  CloseWindow,
-  child = h("div", {style: {minWidth: "300px", minHeight: "150px"}}, text(""))
-) {
-  return h("div", { class: "Window", style: {left: `${x}px`, top: `${y}px`} }, [
-    child,
-    h("div", { class: "Window__title" }, text(title)),
-    h("button", {
-      class: "Window__closer",
-      onclick: CloseWindow,
-      title: "Close",
-    }),
-  ]);
+  onClose,
+  children
+
+}) {
+  return h("div", { className: "Window", style: {
+    left: x, top: y
+  }
+},       
+    children,
+    h("div", {className: "Window__title"}, title),
+    h("button", {className: "Window__closer", title: "Close", })
+   )
 }
