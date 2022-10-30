cde47669afc2134b088f1ed4f21e1355d7fe07a8
vp2177 <vp2177@users.noreply.github.com>
2022-10-30 04:27:24 +0000

    <AboutBox>

---
+++ src/about.js
@@ -0,0 +1,12 @@
+import {h} from "preact"
+
+export function AboutBox() {
+    return h("article", { className:"AboutBox" ,},
+     h("section",null,  `${navigator.hardwareConcurrency} cores`),
+     h("section",{},  navigator.platform),
+     h("section",{},  `${screen.width}*${screen.height}`),
+     h("section",{},  `Came from <${document.referrer}>`),
+     h("section",{},String(navigator.languages) ),
+     
+    )
+}
