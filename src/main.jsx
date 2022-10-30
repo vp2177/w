c6039eaf9b544799ab617209112244c2f4649759
vp2177 <vp2177@users.noreply.github.com>
2022-10-30 10:09:47 +0000

    main -> JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,16 @@
 // import from "preact/compat" // TODO: This crashes SWC
-// import from "react-dom" // TODO: This also crashes SWC
+// import from "react-dom" // TODO: This also crashes SWC (wtf)
 import { render, h } from "preact";
 import { AboutBox } from "./about";
 import { MyWindow } from "./windows";
+/* @jsx h */
 
 function MainApp() {
-  return h(
-    "div",
-    { className: "Desktop" },
-    h(
-      MyWindow,
-      {
-        x: 50,
-        y: 50,
-      },
-      h("div", { className: "Cone" })
-    ),
-    h(
-      MyWindow,
-      {
-        x: 250,
-        y: 20,
-        title: "About",
-      },
-      h(AboutBox)
-    )
-  );
+  return <div className="Desktop">
+    <MyWindow x={50} y={50}><div className="Cone" /></MyWindow>
+    <MyWindow x={250} y={20} title="About"><AboutBox /></MyWindow>
+  </div>
+  
 }
 
 render(h(MainApp), document.getElementById("main"));
