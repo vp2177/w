9b2c7aa1bd4253b9bc1217bd453dcc38af19ef45
vp2177 <vp2177@users.noreply.github.com>
2022-10-30 02:20:59 +0000

    rewrite main app to (P)react

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,26 @@
 // import from "preact/compat" // TODO: This crashes SWC
 // import from "react-dom" // TODO: This also crashes SWC
 import {render, h} from "preact"
-// import { myWindow } from "./windows";
+import {MyWindow} from "./windows"
 
-/*
-app({
-  init: {},
-  view(state) {
-    return h("div", { class: "Desktop" }, [
-      myWindow(10, 10, "Test#1", () => {}),
-    ]);
-  },
-  node: document.getElementById("main"),
-});
-*/
+
+
+
 function MainApp() {
-  return "987"
+  return h("div", {className: "Desktop"},
+    h(MyWindow, {
+      x: 150, y: 20, 
+    }, h("div", {className: "Cone"}) ),
+
+  
+  
+  )
 }
 
 render(h(MainApp), document.getElementById("main"))
+
+
+
 
 
 
