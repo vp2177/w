ee140175b1474477aa51258a4a7e579e622d4aca
vp2177 <vp2177@users.noreply.github.com>
2022-11-06 17:29:52 +0000

    fix another type error

--- src/runtime-info.js
+++ src/runtime-info.js
@@ -1,6 +1,7 @@
+/** @returns {string?} */
 export function getGLRenderer() {
   const el = document.createElement("canvas");
   const ctx = el.getContext("webgl", { failIfMajorPerformanceCaveat: true });
-  const debugInfo = ctx.getExtension("WEBGL_debug_renderer_info");
-  return ctx.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+  const debugInfo = ctx?.getExtension("WEBGL_debug_renderer_info");
+ return (debugInfo)?  ctx?.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL) : "";
 }
