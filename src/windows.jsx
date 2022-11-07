e6e871a34ff4a8cac5a0bc835656fb1394f369f6
vp2177 <vp2177@users.noreply.github.com>
2022-11-07 08:18:59 +0000

    move startTrackingPointerMove() to own file

--- src/windows.jsx
+++ src/windows.jsx
@@ -43,31 +43,3 @@ export function MyWindow({
   );
 }
 
-/**
- * @param onMove {(dx: number, dy: number) => *}
- * @param startingEvent {PointerEvent=}
- * TODO: Move to own file
- */
-export function startTrackingPointerMove(onMove, startingEvent) {
-  const handleMove = (/** @type {PointerEvent} */ ev) => {
-    if (startingEvent)
-      onMove(
-        ev.clientX - startingEvent.clientX,
-        ev.clientY - startingEvent.clientY
-      );
-    else onMove(ev.movementX, ev.movementY);
-  };
-  const handleEnd = (/** @type {PointerEvent} */ ev) => {
-    window.removeEventListener("pointercancel", handleEnd);
-    window.removeEventListener("pointerup", handleEnd);
-    window.removeEventListener("pointermove", handleMove);
-
-    console.info("Stopped tracking due to", ev.type);
-  };
-
-  window.addEventListener("pointercancel", handleEnd);
-  window.addEventListener("pointerup", handleEnd);
-  window.addEventListener("pointermove", handleMove);
-
-  console.info("Started tracking from", startingEvent?.type);
-}
