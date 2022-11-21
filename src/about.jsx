70c7735969446ab899599bcae3c2234c7ae7d340
vp2177 <vp2177@users.noreply.github.com>
2022-11-21 23:50:01 +0000

    convert AboutBox from Preact to Omi

--- src/about.jsx
+++ src/about.jsx
@@ -1,25 +1,30 @@
-import { h } from "preact";
-import { useMemo } from "preact/hooks";
-import { useInterval } from "./hooks";
+import {h, defineElement} from "omi"
+import { ShadowlessElement } from "./omi-utils";
 import { getGLRenderer } from "./runtime-info";
 
-export function AboutBox() {
-  const glRenderer = useMemo(getGLRenderer, []);
+export class AboutBox extends ShadowlessElement {
+
+  _glRenderer = getGLRenderer()
+
+  render() {
+
 
   return (
     <article className="AboutBox">
       <section>{navigator.hardwareConcurrency} cores</section>
       <section>{navigator.platform}</section>
       <section>
-        {screen.width}×{screen.height}
+        {screen.width}*{screen.height}
       </section>
       <section>{String(navigator.languages)}</section>
       <section>
         {navigator.connection?.downlink} down,
         {navigator.connection?.rtt} rtt - {navigator.connection?.type}
       </section>
-      <section>{glRenderer}</section>
+      <section>{this._glRenderer}</section>
       <section>{location.hash}</section>
     </article>
   );
 }
+}
+defineElement("my-about", AboutBox)
