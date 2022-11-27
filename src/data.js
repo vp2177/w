2ddd9150d86871c2bbbee935cd97e79c359e18bb
vp2177 <vp2177@users.noreply.github.com>
2022-11-27 18:38:40 +0000

    implement randomBetween()

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
 export function nextRate(prev = 100, variance = 5 / 100) {
-  const rand = Math.random() * 2 - 1;
+  const rand = randomBetween(-1, 1);
   return prev + rand * variance;
 }
 
 export function randomColorHSL(saturation = 100, light = 50) {
-  const hue = Math.random() * 360;
+  const hue =randomBetween(0, 360) ;
   return `hsl(${hue}, ${saturation}%, ${light}%)`;
+}
+
+export function randomBetween(a = 0, b = 1) {
+ return a + Math.random() * (b-a)
 }
