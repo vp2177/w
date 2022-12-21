ad251caaf3d40b4b0080ee7df7e2104225646fc1
vp2177 <vp2177@users.noreply.github.com>
2022-12-21 22:12:37 +0000

    feat: IPFS protocol test and image format tests

--- src/about.jsx
+++ src/about.jsx
@@ -19,6 +19,30 @@ export function AboutBox() {
       </section>
       <section>{glRenderer}</section>
       <section>{location.hash}</section>
+        <table>
+          <tr>
+            <td title="relative">{location.protocol}</td>
+            <td>ipfs:</td>
+            <td>CF gateway</td>
+          </tr>
+          <tr>
+            <td><img src="assets/teams-tick.png" className="Tick" /></td>
+            <td><img src="ipfs://QmdLRfj7uVNbvW6cb3YaBXyVUEZwmLLVRKWAy1YEsKJX4h"  className="Tick"  /></td>
+            <td><img src="https://cloudflare-ipfs.com/ipfs/QmdLRfj7uVNbvW6cb3YaBXyVUEZwmLLVRKWAy1YEsKJX4h"  className="Tick" /></td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>WebP</td>
+            <td>AVIF</td>
+            <td>JPEG XL</td>
+          </tr>
+          <tr>
+            <td><img src="assets/teams-tick.webp"  className="Tick"  /></td>
+            <td><img src="assets/teams-tick.avif"  className="Tick"  /></td>
+            <td><img src="assets/teams-tick.jxl"  className="Tick"  /></td>
+          </tr>
+        </table>
     </article>
   );
 }
